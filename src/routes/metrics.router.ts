71484294ccd2b421eca57338e2961c35fd0a9541
MuroKuro <murat.k96@hotmail.de>
2024-01-09 17:54:42 +0100

    Update 09.01 17:54 Uhr

--- src/routes/metrics.router.ts
+++ src/routes/metrics.router.ts
@@ -2,10 +2,11 @@ import express = require("express");
 import MetricsController from "../controllers/metrics.controller";
 
 const router = express.Router();
-let message: string = 'Hello World';
-console.log(message);
+
 
 router.post("/", async (req, res) => {
+    let message: string = 'Hello World';
+    console.log(message);
     const controller = new MetricsController();
     const response = await controller.getMetrics(req.body);
     return res.send(response);
