8ed1123356708a614a33207ec0ccdf78e370a12f
MuroKuro <murat.k96@hotmail.de>
2024-01-24 22:20:31 +0100

    24.01

--- src/routes/metrics.router.ts
+++ src/routes/metrics.router.ts
@@ -1,12 +1,22 @@
 import express = require("express");
 import MetricsController from "../controllers/metrics.controller";
+import axios from 'axios';
+import { error } from "console";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
     const controller = new MetricsController();
     const response = await controller.getMetrics(req.body);
-    console.log(response)
+
+    axios.post('http://localhost:8081/src/main/routes/receiveMetrics.ts', { response })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
     return res.send(response);
 });
 
